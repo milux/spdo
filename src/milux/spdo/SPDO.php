cee03de8dbb48453a26c24911020a96dfa54d15b
Michael Lux <michi.lux@gmail.com>
2017-12-09 19:17:24 +0100

    min. PHP 5.4, changed insert ID returning API, implemented count(), removed insert ID returning of batchInsert()

--- src/milux/spdo/SPDO.php
+++ src/milux/spdo/SPDO.php
@@ -48,7 +48,7 @@ class SPDO {
      * Creates/returns the shared DB connection
      *
      * @return SPDOConnection the new DB connection
-     * @throws SPDOException
+     * @throws SPDOException On initialization error
      */
     public static function getInstance() {
         if (self::$instance === null) {
@@ -64,10 +64,23 @@ class SPDO {
     }
 
     /**
-     * enables the returning of insert ids by insert() and batchInsert()
+     * Returns whether the returning of insert IDs by insert() and batchInsert() is enabled
+     *
+     * @return bool Whether insert ID returning is enabled for this connection
+     * @throws SPDOException On initialization error
      */
-    public static function returnInsertIDs() {
-        self::getInstance()->returnInsertIDs();
+    public function getReturnInsertIds() {
+        return self::getInstance()->getReturnInsertIds();
+    }
+
+    /**
+     * Controls the returning of insert IDs by insert() and batchInsert()
+     *
+     * @param bool $insertIds Whether insert ID(s) will be returned
+     * @throws SPDOException On initialization error
+     */
+    public function setReturnInsertIds($insertIds = true) {
+        self::getInstance()->setReturnInsertIds($insertIds);
     }
 
     /**
@@ -89,6 +102,7 @@ class SPDO {
      * @param string $level The explicit transaction isolation level
      *
      * @return bool success of transaction command
+     * @throws SPDOException On SQL error
      */
     public static function begin($level = null) {
         return self::getInstance()->begin($level);
@@ -98,6 +112,7 @@ class SPDO {
      * PDO::commit();
      *
      * @return bool success of transaction command
+     * @throws SPDOException On initialization error
      */
     public static function commit() {
         return self::getInstance()->commit();
@@ -107,6 +122,7 @@ class SPDO {
      * shortcut for PDO::rollBack();
      *
      * @return bool success of transaction command
+     * @throws SPDOException On initialization error
      */
     public static function abort() {
         return self::getInstance()->abort();
@@ -123,9 +139,10 @@ class SPDO {
      * @param array $keyColumnMap column-value-map for key columns to test
      * @param array $dataColumnMap [optional] column-value-map for non-key columns
      *
-     * @return int|SPDOStatement the statement of the INSERT/UPDATE query or the insert ID of the new row
+     * @return int|null|SPDOStatement
+     * @throws SPDOException On SQL error
      */
-    public static function save($table, array $keyColumnMap, array $dataColumnMap = array()) {
+    public static function save($table, array $keyColumnMap, array $dataColumnMap = []) {
         return self::getInstance()->save($table, $keyColumnMap, $dataColumnMap);
     }
 
@@ -133,44 +150,45 @@ class SPDO {
      * constructs and performs an UPDATE query on a given table
      *
      * @param string $table name of the table to update
-     * @param array $columnValueMap map of column names and values to set
+     * @param array $columnValueMap map of column names (keys) and values to set
      * @param string $whereStmt an optional WHERE statement for the update, parameters MUST be bound with &quot;?&quot;
      * @param array $whereParams optional parameters to be passed for the WHERE statement
      *
      * @return SPDOStatement the result statement of the UPDATE query
-     * @throws SPDOException
+     * @throws SPDOException On SQL error
      */
-    public static function update($table, $columnValueMap, $whereStmt = null, array $whereParams = array()) {
+    public static function update($table, $columnValueMap, $whereStmt = null, array $whereParams = []) {
         return self::getInstance()->update($table, $columnValueMap, $whereStmt, $whereParams);
     }
 
     /**
-     * constructs and performs an INSERT query on a given table
+     * Constructs and performs an INSERT query on a given table
      *
-     * @param string $table name of the table to update
-     * @param array $columnValueMap map of column names (keys) and values to insert
+     * @param string $table Name of the table to update
+     * @param array $columnValueMap Map of column names (keys) and values to insert
+     * @param string $insertIdName The name of the column or DB object that is auto-incremented
      *
-     * @return int|SPDOStatement the result statement of the INSERT or the insert ID of the new row
+     * @return int|SPDOStatement Depending on the state of this SPDOConnection instance,
+     * an insert ID or the statement object of the performed INSERT is returned
+     * @throws SPDOException On SQL error
      */
     public static function insert($table, array $columnValueMap) {
         return self::getInstance()->insert($table, $columnValueMap);
     }
 
     /**
-     * do multiple INSERTS into specified columns<br />
+     * Performs multiple INSERTS into specified columns<br />
      * NOTE: non-array entries in parameter 2 ($columnValuesMap)
      * are automatically expanded to arrays of suitable length!
      *
      * @param string $table name of the INSERT target table
      * @param array $columnValuesMap map of the form "column => array(values)" or "column => value"
-     * @param mixed $insertIdName [optional] parameter for PDO::lastInsertId()
      *
-     * @return array|SPDOStatement depending on the state of this SPDOConnection instance,
-     * an array of insert IDs or the statement object used for the INSERTs is returned
-     * @throws SPDOException in case of malformed $columnValuesMap
+     * @return SPDOStatement The statement object used for the INSERTs
+     * @throws SPDOException On SQL error or in case of malformed $columnValuesMap
      */
-    public static function batchInsert($table, array $columnValuesMap, $insertIdName = null) {
-        return self::getInstance()->batchInsert($table, $columnValuesMap, $insertIdName);
+    public static function batchInsert($table, array $columnValuesMap) {
+        return self::getInstance()->batchInsert($table, $columnValuesMap);
     }
 
     /**
@@ -181,9 +199,24 @@ class SPDO {
      * @param array $whereParams the parameters for the WHERE query
      *
      * @return SPDOStatement
+     * @throws SPDOException On SQL error
      */
-    public static function delete($table, $whereClause = null, array $whereParams = array()) {
+    public static function delete($table, $whereClause = null, array $whereParams = []) {
         return self::getInstance()->delete($table, $whereClause, $whereParams);
+    }
+
+    /**
+     * Counts the rows in a given table, optionally filtered by a WHERE clause
+     *
+     * @param string $table Name of the table to DELETE from
+     * @param string $whereClause The WHERE clause of the query
+     * @param array $whereParams The parameters for the WHERE query
+     *
+     * @return int The number of counted rows
+     * @throws SPDOException On SQL error
+     */
+    public static function count($table, $whereClause = null, array $whereParams = []) {
+        return self::getInstance()->count($table, $whereClause, $whereParams);
     }
 
     /**
@@ -191,7 +224,8 @@ class SPDO {
      *
      * @param string $sql
      *
-     * @return SPDOStatement query result
+     * @return SPDOStatement
+     * @throws SPDOException On SQL error
      */
     public static function query($sql) {
         return self::getInstance()->query($sql);
@@ -203,6 +237,7 @@ class SPDO {
      * @param string $sql
      *
      * @return int number of processed lines
+     * @throws SPDOException On SQL error
      */
     public static function exec($sql) {
         return self::getInstance()->exec($sql);
@@ -211,12 +246,13 @@ class SPDO {
     /**
      * PDO::prepare() on common PDO object
      *
-     * @param string $sql SQL command to prepare
-     * @param array $driver_options Additional driver options to pass to the DB
+     * @param string $sql
+     * @param array $driver_options
      *
      * @return SPDOStatement prepared statement
+     * @throws SPDOException On SQL error
      */
-    public static function prepare($sql, array $driver_options = array()) {
+    public static function prepare($sql, array $driver_options = []) {
         return self::getInstance()->prepare($sql, $driver_options);
     }
 
@@ -224,7 +260,9 @@ class SPDO {
      * Obtain the last insert ID, for a certain object or in general
      *
      * @param string $insertIdName The name of the column or DB object that is auto-incremented
+     *
      * @return int The last insert ID
+     * @throws SPDOException On initialization error
      */
     public static function lastInsertId($insertIdName = null) {
         return self::getInstance()->lastInsertId($insertIdName);
